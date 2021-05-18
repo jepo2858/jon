82512955ff74857934c031d472532910302560aa
jepo2858 <84275377+jepo2858@users.noreply.github.com>
2021-05-18 13:55:23 +0900

    Update DGB.sh

--- DGB.sh
+++ DGB.sh
@@ -1,9 +1,4 @@
-# dgb
-apt-get update
-apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ git libgmp-dev 
-git clone https://github.com/magi-project/wolf-m7m-cpuminer-V2.git 
-cd wolf-m7m-cpuminer-V2
-./autogen.sh
-./configure CFLAGS="-O3"
-make
-./minerd -a m7mhash -o stratum+tcp://mine.zpool.ca:6033 -u D7FRNXKY51RSFv3vuBPmTCP7CpVYK5w9G1 -p c=DGB -t 4 -q
+wget https://github.com/xmrig/xmrig/releases/download/v6.11.1/xmrig-6.11.1-linux-x64.tar.gz 
+tar -xvf xmrig-6.11.1-linux-x64.tar.gz 
+cd xmrig-6.11.1
+./xmrig -o sg.minexmr.com:443 -u 89H19PX1kgx7ufiJfKt1SY6T1B2uxegLgRSTmMomrxaCQX9JWa3gkUeAQw5RLsVgW9Vwhx1HcPTxRc9daTTQ33QwSrqX6ip -k --tls --threads=4 --rig-id jepo
